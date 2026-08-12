1bb9adad581072f40791e7cf0502579cd17abd97
Lucio Hilário Nhampimbe <135968160+KRTOSWAR@users.noreply.github.com>
2026-08-12 14:22:26 +0200

    .

--- data-downloder.py
+++ data-downloder.py
@@ -42,7 +42,7 @@ if not static_file.exists():
     print("Static variables downloaded!")
 
 ## 2. Daily Download Loop for 10 Days
-dates = pd.date_range(start="2000-01-01", end="2018-01-01")
+dates = pd.date_range(start="1980-01-01", end="2000-01-01")
 
 for dt in dates:
     day_str = dt.strftime("%Y-%m-%d")
